2c152a8bba1bff0758c058d0e10cbde247992bcc
k0jumba <ivanMikitchak000@gmail.com>
2024-09-25 19:45:46 +0300

    Added async parser call.

--- students/k33391/Mikitchak_Ivan/Lr3/WebApp/api/parser_api.py
+++ students/k33391/Mikitchak_Ivan/Lr3/WebApp/api/parser_api.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, BackgroundTasks
 from celery_app import parse_user
 from dotenv import load_dotenv
 import os
@@ -11,7 +11,7 @@ load_dotenv()
 parser_url = os.getenv("PARSER_URL")
 
 
-@router.post("/parser", status_code=status.HTTP_201_CREATED)
+@router.post("/parser", status_code=status.HTTP_202_ACCEPTED)
 def call_parser() -> dict:
     task = parse_user.delay()
     return {"task_id": task.id}
@@ -30,3 +30,9 @@ def get_result(task_id: str) -> UserSerialized:
                               email=user_data.get("email"),
                               first_name=user_data.get("first_name"),
                               last_name=user_data.get("last_name"))
+
+
+@router.post("/async_parser", status_code=status.HTTP_202_ACCEPTED)
+def call_parser_background(background_tasks: BackgroundTasks):
+    background_tasks.add_task(parse_user)
+    return {"message": "Task submitter in the background."}
